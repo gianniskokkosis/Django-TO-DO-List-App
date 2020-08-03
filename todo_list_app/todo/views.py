f266647c62b52fec426b47be4d16c93de75c25a3
gianniskokkosis <t8160190@aueb.gr>
2020-08-03 11:29:56 +0300

    Fix about view in views.py file
    - Render the about page url with the render method.

--- todo_list_app/todo/views.py
+++ todo_list_app/todo/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home(request):
     return render(request, 'todo/todo_home.html')
 
 
 def about(request):
-    return HttpResponse('<h1>TO-DO List App About Page</h1>')
+    return render(request, 'todo/todo_about.html')
